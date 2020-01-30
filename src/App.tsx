0b5b07938d851f11f10d046b01923a949a542785
Willem Clarke <willem.2@hotmail.com>
2020-01-30 13:12:32 +1000

    richie and I setting up express server

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import * as _ from "lodash";
-
-const App: React.FC = () => {
-  return <h3>reddit-clone-typescript</h3>;
-};
-
-export default App;
---
+++ src/client/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as _ from "lodash";
+
+const App: React.FC = () => {
+  return <h3>reddit-clone-typescript</h3>;
+};
+
+export default App;
