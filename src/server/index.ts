728d9907e0451a11eb79c6bae11e18a516f3c75c
RICHIEm8 <48461113+RICHIEm8@users.noreply.github.com>
2020-02-03 16:02:34 +1100

    mocked routes for doms review

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,10 +5,31 @@ import * as express from "express";
 const app: Application = express();
 const port = process.env.HTTP_PORT || 3000;
 
-app.get("/api/", (req: Request, res: Response) => {
+app.get("/api/posts", (req: Request, res: Response) => {
+  console.info(`This is an API request. Request path is: ${req.path}`);
+  res.status(200).json({
+    hello: "world",
+    post: "get posts"
+  });
+});
+app.get("/api/posts/:id", (req: Request, res: Response) => {
+  console.info(`This is an API request. Request path is: ${req.path}`);
+  res.status(200).json({
+    hello: "world",
+    id: 666
+  });
+});
+app.delete("/api/posts/:id", (req: Request, res: Response) => {
   console.info(`This is an API request. Request path is: ${req.path}`);
   res.status(200).json({
     hello: "world"
+  });
+});
+app.post("/api/posts", (req: Request, res: Response) => {
+  console.info(`This is an API request. Request path is: ${req.path}`);
+  res.status(200).json({
+    title: "title",
+    body: "content"
   });
 });
 
